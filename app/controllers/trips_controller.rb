673e6fc5e2b95b9afd4b149fac17e02fefaba4b4
Geoff <geoff.register@gmail.com>
2019-02-11 16:24:52 -0500

    Trips controller and 2 views created

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,7 @@
+class TripsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'trips/index'
+  get 'trips/new'
+
+  resources :trips
+
+  root 'trips#index'
 end
