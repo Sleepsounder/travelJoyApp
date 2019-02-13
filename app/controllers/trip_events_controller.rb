81256bf239604dddfc79155d3ddcd155e5141d20
Geoff <geoff.register@gmail.com>
2019-02-13 17:27:46 -0500

    added better date formatting and started CRUD for events

--- app/controllers/trip_events_controller.rb
+++ app/controllers/trip_events_controller.rb
@@ -5,6 +5,13 @@ class TripEventsController < ApplicationController
     redirect_to trip_path(@trip)
   end
 
+
+  def destroy
+    @trip = Trip.find(params[:trip_id])
+    @trip_event = @trip.trip_events.find(params[:id])
+    @trip_event.destroy 
+    redirect_to trip_path(@trip)
+
   private
     def trip_event_params
       params.require(:trip_event).permit(:eventname, :eventtime, :location, :journal)
