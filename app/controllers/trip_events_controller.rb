de742069531ea1553d692a7711c92552e338c515
Geoff <geoff.register@gmail.com>
2019-02-13 21:26:55 -0500

    embedded form partial challenges

--- app/controllers/trip_events_controller.rb
+++ app/controllers/trip_events_controller.rb
@@ -1,16 +1,29 @@
 class TripEventsController < ApplicationController
+
   def create
     @trip = Trip.find(params[:trip_id])
     @trip_event = @trip.trip_events.create(trip_event_params)
     redirect_to trip_path(@trip)
   end
 
+  def show
+    @trip = Trip.find(params[:trip_id])
+    @trip_event = @trip.trip_events.find(params[:id])
+    redirect_to edit_trip_trip_event_path(@trip)
+  end
+
+  def edit
+    @trip = Trip.find(params[:trip_id])
+    @trip_event = @trip.trip_events.find(params[:id])
+    redirect_to edit_trip_trip_event_path(@trip)
+  end
 
   def destroy
     @trip = Trip.find(params[:trip_id])
     @trip_event = @trip.trip_events.find(params[:id])
     @trip_event.destroy 
     redirect_to trip_path(@trip)
+  end
 
   private
     def trip_event_params
