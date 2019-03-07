846f7b638ac4735bc2d81a5a6cab41bfdb6e9257
Geoff <geoff.register@gmail.com>
2019-03-07 17:18:45 -0500

    user signup progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new 
+    puts "********new action************"
   end
 
   def create
-    user = User.save
+    @user = User.new(user_params)
+    puts "************create action************"
+
+    if @user.save
+      redirect_to @user
+    else
+      redirect_to new_user_path
+    end 
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
   end
 end
+
